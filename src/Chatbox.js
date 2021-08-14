722df9e108e3bda1a5976d77c4470bf6d8b730cc
m3ntru <zatd39@gmail.com>
2021-08-14 11:49:45 +0800

    version 0.1.2
    - 新增小奇點顯示
    - 修改字體
    - timeout訊息清空

--- src/Chatbox.js
+++ src/Chatbox.js
@@ -8,6 +8,7 @@ export const Chatbox = () => {
   const [messages, setMessges] = useState([
     {
       name: "System",
+      id: "System",
       msg: "initializing...",
       color: "#000000",
       emotes: null
@@ -19,7 +20,7 @@ export const Chatbox = () => {
   }, []);
 
   useEffect(() => {
-    chatBoxref.current.scrollTop = chatBoxref.current.scrollHeight;
+    chatBoxref.current.scrollIntoView({ behavior: "auto" })
   }, [messages]);
 
   const tmiInit = () => {
@@ -40,6 +41,7 @@ export const Chatbox = () => {
       setMessges(preArray => [...preArray,
       {
         name: "System",
+        id: "System",
         msg: "Joined " + paramsId,
         color: "#000000",
         emotes: null
@@ -50,19 +52,40 @@ export const Chatbox = () => {
     });
 
     client.on("message", (target, context, msg, self) => {
-      messageUpdate(target, context, msg, self)
+      messageUpdate(context, msg);
+    });
+
+    client.on("timeout", (channel, username, reason, duration, userstate) => {
+      setMessges(preArray => {
+        preArray = preArray.filter(({ id }) => id !== username);
+        return [...preArray,
+          {
+            name: "timeout",
+            id: "timeout",
+            msg: username + " 吃ㄌ" + duration + "秒的刀",
+            color: "#0000FF",
+            emotes: null
+          }]
+      }
+      )
+    });
+
+    client.on('cheer', (channel, userstate, message) => {
+      var result = Converter.formatText(message, [".", "!", "?", ":", ";", ",", " "], 90, userstate.emotes);
+      messageUpdate(userstate, result.display)
     });
   };
 
-  const messageUpdate = (target, context, msg, self) => {
+  const messageUpdate = (context, msg) => {
     setMessges(preArray => {
-      if (preArray.length === 25) {
+      if (preArray.length === 15) {
         preArray.shift();
       }
       return [...preArray,
       {
         name: context["display-name"],
         msg,
+        id: context.username,
         color: context.color,
         emotes: context.emotes
       }
@@ -72,12 +95,18 @@ export const Chatbox = () => {
 
   return (
     <div className="App">
-      <div ref={chatBoxref} className="chat-box">
+      <div className="chat-box">
         {messages.map((data, index) => (
-          <div className="text" key={index} ><span style={{ fontWeight: "bold", color: data.color, fontSize: "13px" }}>{data.name}:</span>
+          (data.id == "timeout")?
+          <div className="text" style={{fontStyle: "italic", fontSize: "12px", color: "blue"}} key={index}>
+            {parse(Converter.formatTwitchEmotes(data.msg, data.emotes))}
+          </div>
+          :
+          <div className="text" key={index}><span style={{ fontWeight: "bold", color: data.color, fontSize: "14px", marginTop: "14px"}}>{data.name + ": "}</span>
             {parse(Converter.formatTwitchEmotes(data.msg, data.emotes))}
           </div>
         ))}
+        <div ref={chatBoxref} />
       </div>
     </div>
   );
--- src/model/Converter.js
+++ src/model/Converter.js
@@ -1,5 +1,51 @@
 import cheerJson from '../cheer.json'
 class Converter {
+
+    splitTextV1 = (t, e, n, c, v) => {
+        var r = [];
+        function text(o, i) {
+            if (e[i] && o.trim().length)
+                if (o.indexOf(e[i]) > -1) {
+                    var a = 1
+                        , s = o.split(e[i]);
+                    s.forEach((function (o) {
+                        if (o.length) {
+                            var c = "";
+                            a !== s.length && (c = e[i]);
+                            o = (o + c).trim()
+                        }
+                        o.length && o.length <= n ? r.push(o) : text(o, i + 1);
+                        a++
+                    }
+                    ))
+                } else
+                    text(o, i + 1);
+            else if (o.length)
+                for (var c = new RegExp(".{1,".concat(n, "}"), "g"), u = o.match(c); u.length > 0;)
+                    r.push(u.shift().trim())
+        };
+        text(t, 0);
+
+        var out = [];
+        r.forEach(function (t) {
+            out.length && out[out.length - 1].length + t.length + 1 <= n ? out[out.length - 1] += " ".concat(t) : out.push(t)
+        })
+        var cheerRegex = /([0-9])\d+/g;
+        var cheerList = c.match(cheerRegex);
+        var cheerTotal = 0;
+        if (cheerList) {
+            cheerList.forEach(function (t) {
+                cheerTotal = cheerTotal + parseInt(t);
+            })
+        }
+        var data = {
+            count: cheerTotal,
+            display: v,
+            message: out
+        }
+        return data;
+    }
+
     
     formatText = (t, e, n, z) => {
         var space = /\s+/g;
@@ -9,7 +55,7 @@ class Converter {
         //t = t.replace(comma, '');
         var u = vv,
             l = t,
-            f = ["cheer", "doodlecheer", "biblethump", "cheerwhal", "corgo", "scoops", "party", "seemsgood", "pride", "uni", "showlove", "kappa", "frankerz", "heyguys", "dansgame", "elegiggle", "trihard", "kreygasm", "4head", "swiftrage", "notlikethis", "failfish", "vohiyo", "pjsalt", "mrdestructoid", "bday", "ripcheer", "shamrock", "bitboss", "streamlabs", "muxy", "holidaycheer", "goal", "anon", "charity", "tgm3cheer"]
+            f = ["cheer", "doodlecheer", "biblethump", "cheerwhal", "corgo", "scoops", "party", "seemsgood", "pride", "uni", "showlove", "kappa", "frankerz", "heyguys", "dansgame", "elegiggle", "trihard", "kreygasm", "4head", "swiftrage", "notlikethis", "failfish", "vohiyo", "pjsalt", "mrdestructoid", "bday", "ripcheer", "shamrock", "bitboss", "streamlabs", "muxy", "holidaycheer", "goal", "anon", "charity", "ickidcheer"]
         u.length > 300 && (u = "".concat(u.substring(0, 296), "..."));
         l.length > 550 && (l = "".concat(l.substring(0, 446), "..."));
         var d = new RegExp("\\b(".concat(f.join("|"), ")([0-9]+)\\b"), "gi"),
@@ -33,7 +79,7 @@ class Converter {
             if (parseInt(t[3]) >= 1 && parseInt(t[3]) <= 99) { color = "grey"; height = 1; }
             var temp1 = t[2].toLowerCase();
             var temp2 = cheerJson[temp1][height];
-            v = v.replace(t[0], "<img src='" + temp2 + "' /><font color='" + color + "'>" + t[3] + "</font>");
+            v = v.replace(t[0], "<img class='cheer' src='" + temp2 + "' />");
             b = b.replace(t[0], "");
             c = c + t[0] + ' ';
         }))
